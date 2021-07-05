9321f6db370a955930f58074be980640efb9f685
KushKushStudent <1882078@students.wits.ac.za>
2021-07-05 14:14:23 +0200

    api

---
+++ JsScripts/jokes.js
@@ -0,0 +1,16 @@
+console.log(Object.values(JokeAPI));
+
+
+JokeAPI.getJokes({
+  jokeType: "single"
+})
+  .then((r) => r.json())
+  .then((data) => {
+    updateUI(data);
+  });
+
+function updateUI(jokeData) {
+  const $ = (id) => document.getElementById(id);
+
+  $("joke--text").innerHTML = jokeData.joke;
+}
